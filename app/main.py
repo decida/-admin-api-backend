7b1f4b75d9b5097f7776915e29fc58357e4a219b
ddias-olv <ddias.olv@gmail.com>
2025-11-27 10:53:48 -0300

    set the all cors

--- app/main.py
+++ app/main.py
@@ -41,10 +41,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",  # Vite dev server
-        "http://127.0.0.1:5173",  # Alternative localhost,
-        "http://decida-admin-api-frontend.woelbp.easypanel.host",
-        "http://decida-admin-api-frontend.wkmxos.easypanel.host"
+        "*"
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
